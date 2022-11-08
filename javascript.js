9ba011f9da3164d5f298aac636e172a97940759e
Gonzalo <107971962+gonza2323@users.noreply.github.com>
2022-11-07 23:06:25 -0300

    Add button for resizing grid and refactoring

--- javascript.js
+++ javascript.js
@@ -1,15 +1,23 @@
 "use strict";
 
-const container = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
-    for (let i = 0; i < 16; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.addEventListener('mouseover', e => e.target.classList.add('paintedSquare'));
-        row.appendChild(square);
+const body = document.querySelector('body');
+
+function makeGrid(size) {
+    const container = document.createElement('div');
+    container.classList.add('.container');
+    body.appendChild(container);
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (let i = 0; i < size; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.addEventListener('mouseover', e => e.target.classList.add('paintedSquare'));
+            row.appendChild(square);
+        }
     }
 }
+
+makeGrid(16);
