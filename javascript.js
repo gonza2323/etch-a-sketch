88d8a479657b7b5fd58dc1d1e91d00a806d86d6b
Gonzalo <107971962+gonza2323@users.noreply.github.com>
2022-11-07 22:50:07 -0300

    Create 16x16 square grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
+    for (let i = 0; i < 16; i++) {
+        const square = document.createElement('span');
+        square.classList.add('square');
+        row.appendChild(square);
+    }
+}
